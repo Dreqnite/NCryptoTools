b592e1365ce0220f8e3cfa29c58a06b19c0d99cb
Dreqnite <AndrewKrylovNegovsky@gmail.com>
2018-05-21 17:51:26 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Module for NCryptoTools package distribution.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='NCryptoTools',
+    version='0.5.2',
+    packages=find_packages(where='.', exclude=['test_*']),
+    description='A set of tools needed for both NCryptoClient and NCryptoServer applications',
+    author='Andrew Krylov',
+    author_email='AndrewKrylovNegovsky@gmail.com',
+    license='GNU General Public License v3.0',
+    keywords=['JSON', 'Logger', 'Tools', 'FileManager', 'Utilities'],
+)
